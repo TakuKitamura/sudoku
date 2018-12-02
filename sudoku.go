eb25b51e7edf059d79b01764483c6eb58efbc90d
TakuKitamura <tikutaku0128@icloud.com>
2018-12-02 17:20:08 +0900

    add rc, rn, cn, bn map

--- sudoku.go
+++ sudoku.go
@@ -21,39 +21,28 @@ func sudokuSolve(grid [9][9]int) {
 	keyNames := [4]string{"rc", "rn", "cn", "bn"}
 	nameIndex := 0
 
-	j := 0
+	j := -1
 
-	for i := 0; i < 81; i++ {
-		k := i % N
-		if i != 0 && k == 0 {
-			j++
+	for i := 0; i < len(X); i++ {
+		var k int
+		if nameIndex == 0 {
+			k = i % N
+			if k == 0 {
+				j++
+			}
+		} else {
+			k = (i % N) + 1
+			if k == 1 {
+				j++
+			}
 		}
-		X[i].KeyName = keyNames[nameIndex]
 		X[i].Values = [2]int{j, k}
-	}
-
-	nameIndex++
-
-	j = 0
-	for i := 81; i < len(X); i++ {
-		k := (i % N) + 1
-
-		if i != 0 && k == 1 {
-			j++
-		}
-
 		X[i].KeyName = keyNames[nameIndex]
-		X[i].Values = [2]int{j - 1, k}
-
-		if i == 160 {
+		if i == 80 || i == 160 || i == 240 {
 			nameIndex++
-			j = 0
-		} else if i == 240 {
-			nameIndex++
-			j = 0
+			j = -1
 		}
 	}
-
 }
 
 func main() {
