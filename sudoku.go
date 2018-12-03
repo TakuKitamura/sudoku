9eb28e8ad473b207a9116c8d467a96b9b7f42c4a
TakuKitamura <tikutaku0128@icloud.com>
2018-12-03 12:58:47 +0900

    add choice func

--- sudoku.go
+++ sudoku.go
@@ -23,19 +23,19 @@ type YStruct struct {
 	N int
 }
 
-func exactCover(X [324]XStruct, Y map[YStruct][4]XStruct) {
+func exactCover(X [324]XStruct, Y map[YStruct][4]XStruct) map[XStruct][9]YStruct {
 
 	Z := map[XStruct][9]YStruct{}
 	i := 0
-	ystruct := [9]YStruct{}
+	yStruct := [9]YStruct{}
 
 	for _, v := range X { // ok
 		for key, vv := range Y { // ok
 			for _, vvv := range vv { //ok
 				if v == vvv {
-					ystruct[i] = key
+					yStruct[i] = key
 					if i == 8 {
-						Z[v] = ystruct
+						Z[v] = yStruct
 						i = 0
 					} else {
 						i++
@@ -45,8 +45,36 @@ func exactCover(X [324]XStruct, Y map[YStruct][4]XStruct) {
 		}
 	}
 
-	fmt.Println(Z)
+	return Z
 }
+
+func choice(Z map[XStruct][9]YStruct, Y map[YStruct][4]XStruct, r YStruct) {
+	for _, v := range Y[r] {
+		for _, vv := range Z[v] {
+			for _, vvv := range Y[vv] {
+				if vvv != v {
+					ZV := Z[vvv]
+					for i := 0; i < len(ZV); i++ {
+						if ZV[i] == vv {
+							ZV[i] = YStruct{0, 0, 0}
+							break
+						}
+					}
+					Z[vvv] = ZV
+				}
+			}
+
+		}
+	}
+}
+
+// def select(X, Y, r):
+//     for j in Y[r]:
+//         for i in X[j]:
+//             for k in Y[i]:
+//                 if k != j:
+//                     X[k].remove(i)
+//         cols.append(X.pop(j))
 
 func sudokuSolve(grid [9][9]int) {
 	R, C := 3, 3
@@ -109,8 +137,37 @@ func sudokuSolve(grid [9][9]int) {
 		}
 	}
 
+	// for i, row in enumerate(grid):
+	// for j, n in enumerate(row):
+
+	Z := exactCover(X, Y)
+
+	for i, row := range grid {
+		fmt.Println(i, row)
+		for j, n := range row {
+			if n != 0 {
+				yStruct := YStruct{i, j, n}
+				// fmt.Println(yStruct)
+				choice(Z, Y, yStruct)
+				// fmt.Println(Z, yStruct)
+			}
+		}
+	}
+	fmt.Println(Z)
+
 }
 
 func main() {
-
+	grid := [9][9]int{
+		{0, 6, 1, 0, 0, 7, 0, 0, 3},
+		{0, 9, 2, 0, 0, 3, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 8, 5, 3, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 5, 0, 4},
+		{5, 0, 0, 0, 0, 8, 0, 0, 0},
+		{0, 4, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 1, 6, 0, 8, 0, 0},
+		{6, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	sudokuSolve(grid)
 }
