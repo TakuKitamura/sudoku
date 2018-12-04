d197a744754e9797fd02c5a4317fc4531ac0f14c
TakuKitamura <tikutaku0128@icloud.com>
2018-12-04 09:52:07 +0900

    fix logig

--- sudoku.go
+++ sudoku.go
@@ -4,12 +4,6 @@ import (
 	"fmt"
 )
 
-// Y[(r, c, n)] = [
-// 	("rc", (r, c)),
-// 	("rn", (r, n)),
-// 	("cn", (c, n)),
-// 	("bn", (b, n))]
-
 type XStruct struct {
 	KeyName string
 	Values  [2]int
@@ -24,14 +18,13 @@ type YStruct struct {
 }
 
 func exactCover(X [324]XStruct, Y map[YStruct][4]XStruct) map[XStruct][9]YStruct {
-
 	Z := map[XStruct][9]YStruct{}
 	i := 0
 	yStruct := [9]YStruct{}
 
-	for _, v := range X { // ok
-		for key, vv := range Y { // ok
-			for _, vvv := range vv { //ok
+	for _, v := range X {
+		for key, vv := range Y {
+			for _, vvv := range vv {
 				if v == vvv {
 					yStruct[i] = key
 					if i == 8 {
@@ -44,25 +37,16 @@ func exactCover(X [324]XStruct, Y map[YStruct][4]XStruct) map[XStruct][9]YStruct
 			}
 		}
 	}
-
 	return Z
 }
 
-// def select(X, Y, r):
-//     cols = []
-//     for j in Y[r]:
-//         for i in X[j]:
-//             for k in Y[i]:
-//                 if k != j:
-//                     X[k].remove(i)
-//         cols.append(X.pop(j))
-//     print(cols)
-//     return cols
-
 func choice(Z map[XStruct][9]YStruct, Y map[YStruct][4]XStruct, r YStruct) {
-	// cols := make([][9]YStruct, len(Y[r]))
+	isNullYStruct := YStruct{0, 0, 0}
 	for _, v := range Y[r] {
 		for _, vv := range Z[v] {
+			if vv == isNullYStruct {
+				continue
+			}
 			for _, vvv := range Y[vv] {
 				if vvv != v {
 					ZV := Z[vvv]
@@ -77,98 +61,49 @@ func choice(Z map[XStruct][9]YStruct, Y map[YStruct][4]XStruct, r YStruct) {
 			}
 		}
 		delete(Z, v)
-		// if isReturnedPop == false {
-		// 	delete(Z, v)
-		// } else {
-		// 	cols[i] = Z[v]
-		// }
 	}
-
-	// if isReturnedPop == false {
-	// 	return nil
-	// } else {
-	// 	return cols
-	// }
 }
-
-// def solve(X, Y, solution):
-//     if not X:
-//         yield list(solution)
-//     else:
-//         c = min(X, key=lambda c: len(X[c]))
-//         for r in list(X[c]):
-//             solution.append(r)
-//             cols = select(X, Y, r)
-//             for s in solve(X, Y, solution):
-//                 yield s
-//             deselect(X, Y, r, cols)
-//             solution.pop()
 
 func solve(Z map[XStruct][9]YStruct, Y map[YStruct][4]XStruct, solution []YStruct) {
 	i := 0
-	// fmt.Println(Z)
 	for i < len(solution) {
 		min := 10
 		c := XStruct{}
 		counter := 0
 		isNullYStruct := YStruct{0, 0, 0}
-		// fmt.Println(Z)
 		for key, v := range Z {
-			// fmt.Println(key, v)
 			for _, vv := range v {
 				if vv != isNullYStruct {
 					counter++
 				}
 			}
-			if counter == 0 {
-				continue
-			}
-			fmt.Println(counter, min, key, v)
 			if counter < min {
 				min = counter
 				c = key
-
 				if min == 1 {
 					break
 				}
-			} else {
-				fmt.Println(counter, min)
 			}
 			counter = 0
 		}
-		// fmt.Println()
-
-		// if min == 0 {
-		// 	break
-		// }
-		// fmt.Println(min, c, Z[c])
-		// fmt.Println()
 		r := YStruct{}
-		// fmt.Println(c, Z[c])
 		for _, v := range Z[c] {
 			if v != isNullYStruct {
 				r = v
 				break
 			}
 		}
-		// r := Z[c][0]
 		solution[i] = r
-		// fmt.Println(solution)
 		choice(Z, Y, r)
 		i++
-
 	}
 }
 
 func sudokuSolve(grid [9][9]int) [9][9]int {
 	R, C := 3, 3
-	// fmt.Println(R, C)
 	N := 9
-
 	X := [324]XStruct{} // [N * N * 4]
-
 	keyNames := [4]string{"rc", "rn", "cn", "bn"}
-
 	nameIndex := 0
 	k := 0
 	for i := 0; i < N; i++ {
@@ -178,7 +113,6 @@ func sudokuSolve(grid [9][9]int) [9][9]int {
 			k++
 		}
 	}
-
 	for h := 0; h < 3; h++ {
 		nameIndex++
 		for i := 0; i < N; i++ {
@@ -189,9 +123,7 @@ func sudokuSolve(grid [9][9]int) [9][9]int {
 			}
 		}
 	}
-
 	Y := map[YStruct][4]XStruct{}
-
 	for h := 0; h < N; h++ {
 		for i := 0; i < N; i++ {
 			for j := 1; j < N+1; j++ {
@@ -214,20 +146,12 @@ func sudokuSolve(grid [9][9]int) [9][9]int {
 				bn.Values = [2]int{b, j}
 
 				Y[yStruct] = [4]XStruct{rc, rn, cn, bn}
-				// yStruct.R = h
-				// yStruct.C = i
-				// yStruct.N = j
 			}
 		}
 	}
-
-	// for i, row in enumerate(grid):
-	// for j, n in enumerate(row):
-
 	Z := exactCover(X, Y)
 	zeroCount := 0
 	for i, row := range grid {
-		// fmt.Println(i, row)
 		for j, n := range row {
 			if n == 0 {
 				zeroCount++
@@ -237,23 +161,12 @@ func sudokuSolve(grid [9][9]int) [9][9]int {
 			}
 		}
 	}
-	// fmt.Println(Z)
 	solution := make([]YStruct, zeroCount)
-	// fmt.Println(solution)
 	solve(Z, Y, solution)
-	fmt.Println(solution)
 	for _, v := range solution {
 		grid[v.R][v.C] = v.N
 	}
 	return grid
-
-	// 	for (r, c, n) in a:
-	// 	grid[r][c] = n
-	// return grid
-
-	// solution := make([]YStruct, zeroCount)
-	// fmt.Println(solution)
-
 }
 
 func main() {
@@ -268,5 +181,6 @@ func main() {
 		{0, 0, 0, 1, 6, 0, 8, 0, 0},
 		{6, 0, 0, 0, 0, 0, 0, 0, 0},
 	}
-	fmt.Println(sudokuSolve(grid))
+	results := sudokuSolve(grid)
+	fmt.Println(results)
 }
