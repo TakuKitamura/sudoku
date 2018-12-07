91c0d683f0d2ff7703dbce0f61dfbcc3664a5ff1
TakuKitamura <tikutaku0128@icloud.com>
2018-12-07 15:08:11 +0900

    complete sudokuGenerate func

--- src/api/sudoku_generate.go
+++ src/api/sudoku_generate.go
@@ -11,7 +11,7 @@ func rundomValue(n int) int {
 	return rand.Intn(n)
 }
 
-func sudokuGenerate() {
+func sudokuGenerate() (answer [9][9]uint8) {
 	seedSudoku := [9][9]uint8{
 		{4, 6, 1, 9, 8, 7, 2, 5, 3},
 		{7, 9, 2, 4, 5, 3, 1, 6, 8},
@@ -24,43 +24,53 @@ func sudokuGenerate() {
 		{6, 1, 7, 8, 4, 2, 9, 3, 5},
 	}
 
-	// exchange
-	// 0: 0and1
-	// 1: 0and2
-	// 2: 1and2
+	temp := seedSudoku
 
-	for i := 0; i < 50; i++ {
-		boxNum := uint8(rundomValue(3))
-		exchangeSeed := rundomValue(3)
-		var a uint8
-		var b uint8
-		if exchangeSeed == 0 {
-			a = 0 + 3*boxNum
-			b = 1 + 3*boxNum
-		} else if exchangeSeed == 1 {
-			a = 0 + 3*boxNum
-			b = 2 + 3*boxNum
-		} else {
-			a = 1 + 3*boxNum
-			b = 2 + 3*boxNum
+	for {
+		// exchange
+		// 0: 0and1
+		// 1: 0and2
+		// 2: 1and2
+		for i := 0; i < 50; i++ {
+			boxNum := uint8(rundomValue(3))
+			exchangeSeed := rundomValue(3)
+			var a uint8
+			var b uint8
+			if exchangeSeed == 0 {
+				a = 0 + 3*boxNum
+				b = 1 + 3*boxNum
+			} else if exchangeSeed == 1 {
+				a = 0 + 3*boxNum
+				b = 2 + 3*boxNum
+			} else {
+				a = 1 + 3*boxNum
+				b = 2 + 3*boxNum
+			}
+			var c uint8
+			var d uint8
+			if rundomValue(2) == 0 {
+				c = a
+				d = b
+			} else {
+				c = b
+				d = a
+			}
+			for j := 0; j < 9; j++ {
+				tempVal := seedSudoku[c][j]
+				seedSudoku[c][j] = seedSudoku[d][j]
+				seedSudoku[d][j] = tempVal
+			}
 		}
-
-		var c uint8
-		var d uint8
-		if rundomValue(2) == 0 {
-			c = a
-			d = b
-		} else {
-			c = b
-			d = a
+		for i := 0; i < 65; i++ {
+			j := rundomValue(9)
+			k := rundomValue(9)
+			seedSudoku[j][k] = 0
 		}
-
-		for j := 0; j < 9; j++ {
-			tempVal := seedSudoku[c][j]
-			seedSudoku[c][j] = seedSudoku[d][j]
-			seedSudoku[d][j] = tempVal
+		fmt.Println(seedSudoku)
+		answer, _, _, err := sudokuSolve(seedSudoku)
+		if err == nil {
+			return answer
 		}
-
+		seedSudoku = temp
 	}
-	fmt.Println(seedSudoku)
 }
